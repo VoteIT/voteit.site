f737fd89dce19bc0d98172b1edb168d92b0efebf
Fredrik Nilson <sassur@gmail.com>
2012-02-09 23:17:39 +0100

    fix support tab to send email

--- voteit/site/views/components/help_actions.py
+++ voteit/site/views/components/help_actions.py
@@ -3,13 +3,20 @@ from pyramid.url import resource_url
 from betahaus.viewcomponent import view_action
 
 from voteit.site import SiteMF as _
+from voteit.site.views.help import HelpView
 
 
 @view_action('help_action', 'support')
 def action_support(context, request, va, **kw):
     api = kw['api']
     if api.root:
-        link = resource_url(api.root, request) + "support"
-        return """<li><a id="support" class="tab buttonize" href="%s">%s</a></li>""" % (link, api.translate(_(u"Support")))
-        
+        return """<li><a class="tab buttonize" href="#help-tab-support">%s</a></li>""" % (api.translate(_(u"Support")),)
+    return ""
+
+@view_action('help_tab', 'support')
+def tap_support(context, request, va, **kw):
+    api = kw['api']
+    if api.root:
+        hw = HelpView(api.root, request)
+        return """<div id="help-tab-support" class="tab">%s</div>""" % (hw.support(),)
     return ""
--- voteit/site/views/help.py
+++ voteit/site/views/help.py
@@ -5,6 +5,10 @@ from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound
 from pyramid.httpexceptions import HTTPRedirection
 from pyramid.url import resource_url
+from pyramid.renderers import render
+from pyramid.response import Response
+from pyramid_mailer import get_mailer
+from pyramid_mailer.message import Message
 from betahaus.pyracont.factories import createSchema
 
 from voteit.core import security
@@ -13,33 +17,55 @@ from voteit.core.models.interfaces import ISiteRoot
 from voteit.core.models.interfaces import IMeeting
 from voteit.core.models.schemas import add_csrf_token
 from voteit.core.models.schemas import button_send
+from voteit.core import fanstaticlib
 
 from voteit.site import SiteMF as _
 
 class HelpView(BaseView):
-    @view_config(name = 'support', context=ISiteRoot, renderer="templates/ajax_edit.pt", permission=NO_PERMISSION_REQUIRED)
-    def contact(self):
+    @view_config(name = 'support', context=ISiteRoot, permission=NO_PERMISSION_REQUIRED)
+    def support(self):
         """ Support form
         """
         schema = createSchema('SupportSchema').bind(context=self.context, request=self.request)
         add_csrf_token(self.context, self.request, schema)
             
-        form = Form(schema, buttons=(button_send,))
+        form = Form(schema, action=resource_url(self.context, self.request)+"@@support", buttons=(button_send,), formid="help-tab-support-form", use_ajax=True)
         self.api.register_form_resources(form)
 
         post = self.request.POST
 
-        if 'send' in post:
+        if self.request.method == 'POST':
             controls = post.items()
             try:
                 appstruct = form.validate(controls)
             except ValidationFailure, e:
                 self.response['form'] = e.render()
-                return self.response
+                return Response(render("templates/ajax_edit.pt", self.response, request = self.request))
             
-            #FIXME: send message to moderators
-            self.api.flash_messages.add(_(u"Message sent to VoteIT"))
+            sender = "VoteIT <info@voteit.se>"
+
+            recipients = ("info@voteit.se",) 
+
+            response = {
+                        'api': self.api,
+                        'meeting': self.api.meeting,
+                        'user': self.api.get_user(self.api.userid),
+                        'subject': appstruct['subject'],
+                        'message': appstruct['message'],
+                        }
+            body_html = render('templates/email/help_support.pt', response, request=self.request)
+        
+            msg = Message(subject=_(u"VoteIT - Support request"),
+                          sender = sender and sender or None,
+                          recipients=recipients,
+                          html=body_html)
+        
+            mailer = get_mailer(self.request)
+            mailer.send(msg)
+            
+            self.response['message'] = _(u"Message sent to VoteIT")
+            return Response(render("templates/ajax_success.pt", self.response, request = self.request))
 
         #No action - Render form
         self.response['form'] = form.render()
-        return self.response
+        return render("templates/ajax_edit.pt", self.response, request = self.request)
